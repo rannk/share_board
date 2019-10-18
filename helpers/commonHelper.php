4b2ea201e5c7233bd905dddfd096fb56e9e577f4
rannk <rannk@163.com>
2019-10-18 13:42:53 +0800

    up link

--- helpers/commonHelper.php
+++ helpers/commonHelper.php
@@ -1,6 +1,6 @@
 <?php
 function addAtagForLink($content) {
-    $pattern = '/(http[s]{0,1}:\/\/[\w\d\.\-_\/]{1,})/i';
+    $pattern = '/(http[s]{0,1}:\/\/[\w\d\.\-_\/#\?\%:=]{1,})/i';
     $replacement = '<a href="${1}" target=_blank>${1}</a>';
     return preg_replace($pattern, $replacement, $content);
 }

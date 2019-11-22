ce2f8dea662183daaa30f4dbc5e6504a130505d9
rannk <rannk@163.com>
2019-11-22 10:38:33 +0800

    improve modal

--- js/modal.js
+++ js/modal.js
@@ -2,18 +2,16 @@
 (function ($) {
     $.fn.modal = function (action) {
         var target = this;
-        var s = false;
         if(action == "show") {
             $(target).offset({"top":$("#panel").height()/2-$(target).height()/2, "left": $("#panel").width()/2-$(target).width()/2});
             $(target).show();
             $("body").unbind("click");
             $("body").on("click",function (e) {
-                if($(e.target).closest(target).length == 0 && s ==  true) {
+                if($(e.target).closest(target).length == 0) {
                     $(target).offset({"top":0,"left":0});
                     $(target).hide();
                     $("body").unbind("click");
                 }
-                s = true;
             });
         }
 

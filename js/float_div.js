af29d2b988a9e7abecebeb644e67e934bff174fc
rannk <rannk@163.com>
2019-11-29 16:43:15 +0800

    improve float_div js

--- js/float_div.js
+++ js/float_div.js
@@ -2,16 +2,23 @@
  * 根据点击元素的位置显示层
  */
 (function ($) {
-    $.fn.floatDiv = function (target, action, offset) {
-        var f_top, f_left, f_width, w_width, w_height, d_width, d_height, is_show=false;
-        f_top = $(this).offset().top;
-        f_left = $(this).offset().left;
-        f_width = $(this).width();
+    $.fn.floatDiv = function (action, target, offset) {
+        var f_top = 0, f_left = 0, f_width, w_width, w_height, d_width, d_height, float_div;
+
+        float_div = this;
+
+        if($(target).length > 0) {
+            f_top = $(target).offset().top;
+            f_left = $(target).offset().left;
+        }
+
+        f_width = $(target).width();
         w_width = window.innerWidth;
         w_height = window.innerHeight;
-        d_width = $(target).width();
-        d_height = $(target).height();
-        click_target = this;
+        d_width = $(float_div).width();
+        d_height = $(float_div).height();
+        click_target = $(target);
+
 
         if(typeof offset == "object") {
             if(offset.top > 0) {
@@ -21,6 +28,7 @@
                 f_left += offset.left;
             }
         }
+
         f_left = f_width + f_left;
         // 如果层超过窗口的右面，则把层放置到元素的左面
         if(f_left + d_width > w_width){
@@ -41,20 +49,22 @@
         }
 
         if(action == "show") {
-            $(target).offset({"top":f_top,"left":f_left});
-            $(target).show();
+            $(float_div).offset({"top":f_top,"left":f_left});
+            $(float_div).show();
+            $("body").unbind("click");
             $("body").on("click",function (e) {
-                if($(e.target).closest(target).length == 0 && $(e.target).closest(click_target).length == 0) {
-                    $(target).offset({"top":0,"left":0});
-                    $(target).hide();
+                if($(e.target).closest(float_div).length == 0 && $(e.target).closest(click_target).length == 0) {
+                    $(float_div).offset({"top":0,"left":0});
+                    $(float_div).hide();
                     $("body").unbind("click");
                 }
             });
         }
 
         if(action == "hide") {
-            $(target).offset({"top":0,"left":0});
-            $(target).hide();
+            $(float_div).offset({"top":0,"left":0});
+            $(float_div).hide();
+            $("body").unbind("click");
         }
     }
 }(jQuery));
